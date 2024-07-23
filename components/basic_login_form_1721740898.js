63dc85340ade12837083c5a25721eb35722b7c88
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-23 13:22:45 +0000

    Code generation
    {
        "level": 1,
        "task_description": "add radio button for gender "
    }

--- components/basic_login_form_1721740898.js
+++ components/basic_login_form_1721740898.js
@@ -19,6 +19,24 @@ Vue.component('basic_login_form_1721740898', {
               <label class="font-medium">Password</label>
               <input type="password" required class="w-full mt-2 px-3 py-2 text-black bg-transparent outline-none border border-gray-300 focus:border-purple-600 shadow-sm rounded-lg" />
             </div>
+            <div>
+              <label class="font-medium">Gender</label>
+              <div class="mt-2 flex space-x-4">
+                <label class="inline-flex items-center">
+                  <input type="radio" name="gender" value="male" class="form-radio text-green-600" />
+                  <span class="ml-2">Male</span>
+                </label>
+                <label class="inline-flex items-center">
+                  <input type="radio" name="gender" value="female" class="form-radio text-green-600" />
+                  <span class="ml-2">Female</span>
+                </label>
+                <label class="inline-flex items-center">
+                  <input type="radio" name="gender" value="other" class="form-radio text-green-600" />
+                  <span class="ml-2">Other</span>
+                </label>
+              </div>
+            </div>
+            </div>
             <button class="w-full px-4 py-2 text-white font-medium bg-green-600 hover:bg-green-500 active:bg-green-600 rounded-lg duration-150">Sign in</button>
             <div class="text-center">
               <button class="bg-red-500 hover:bg-red-400 active:bg-yellow-500 text-white font-medium text-sm p-3 rounded-lg">Forgot password?</button>
